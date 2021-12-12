74eeaa3bf0b4e329efe80ffab8d34ad49f4934aa
Parker L <leonard.parker5@gmail.com>
2021-12-11 20:38:13 -0500

    Day 1 Part 2 Solution
    Day 1 Part 2 Solution

--- day1.py
+++ day1.py
@@ -14,3 +14,12 @@ for i in range(len(depthlist) - 1):
 print("depthcounter is " + str(depthcounter))
 
 #PART 2
+
+groupcounter = 0
+for i in range(len(depthlist) - 3):
+    blockA = (depthlist[i] + depthlist[i + 1] + depthlist[i + 2])
+    blockB = (depthlist[i + 1] + depthlist[i + 2] + depthlist[i + 3])
+    if(blockB > blockA):
+        groupcounter += 1
+    
+print("groupcounter is " + str(groupcounter))
